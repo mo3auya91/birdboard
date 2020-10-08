595aa767aa8e80afa97f744d115d0abf8549a8a0
Moauya Meghari <moauya@ns3a.com>
2020-10-08 11:04:11 +0300

    install inertia

--- public/js/app.js
+++ public/js/app.js
@@ -42224,8 +42224,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Volumes/Data/Sites/birdboard/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Volumes/Data/Sites/birdboard/resources/css/app.css */"./resources/css/app.css");
+__webpack_require__(/*! /Volumes/Data/Sites/l8-inertia/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Volumes/Data/Sites/l8-inertia/resources/css/app.css */"./resources/css/app.css");
 
 
 /***/ })
