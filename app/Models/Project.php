44bfd16915886f9a9f7e365ea50ace48ce518c0e
Moauya Meghari <moauya@ns3a.com>
2020-10-16 18:42:45 +0300

    install ide-helper package

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,9 +2,32 @@
 
 namespace App\Models;
 
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\Project
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $description
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property string|null $deleted_at
+ * @method static Builder|Project newModelQuery()
+ * @method static Builder|Project newQuery()
+ * @method static Builder|Project query()
+ * @method static Builder|Project whereCreatedAt($value)
+ * @method static Builder|Project whereDeletedAt($value)
+ * @method static Builder|Project whereDescription($value)
+ * @method static Builder|Project whereId($value)
+ * @method static Builder|Project whereTitle($value)
+ * @method static Builder|Project whereUpdatedAt($value)
+ * @mixin Eloquent
+ */
 class Project extends Model
 {
     use HasFactory;
